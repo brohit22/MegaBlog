d16336d788135b40764f0595bf5cda6391086c3c
Mr-Mic <bhatade.2114501587@mujonline.edu.in>
2024-02-19 10:27:39 +0530

    First commit

---
+++ src/components/index.js
@@ -0,0 +1,31 @@
+import Header from './Header/Header';
+import Footer from './Footer/Footer';
+import Container from './container/Container';
+import Input from './Input';
+import Logo from './Logo';
+import Button from './Button';
+import LogoutBtn from './Header/LogoutBtn';
+import PostCard from './PostCard';
+import PostForm from './Post/PostForm';
+import Select from './Select';
+import Login from './Login';
+import SignUp from './SignUp';
+import RTE from './RTE';
+import AuthLayout from './AuthLayout';
+
+export {
+  Header,
+  Footer,
+  Container,
+  Input,
+  Logo,
+  Button,
+  LogoutBtn,
+  PostCard,
+  PostForm,
+  Select,
+  Login,
+  SignUp,
+  RTE,
+  AuthLayout,
+};
